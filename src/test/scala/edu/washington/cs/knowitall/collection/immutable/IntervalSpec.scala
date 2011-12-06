63b4db706a6322cad565c7f55ce872ea2d0d81a4
Michael Schmitz <schmmd@cs.washington.edu>
2011-12-06 09:50:22 -0800

    Added test cases for interval.

--- src/test/scala/edu/washington/cs/knowitall/collection/immutable/IntervalSpec.scala
+++ src/test/scala/edu/washington/cs/knowitall/collection/immutable/IntervalSpec.scala
@@ -16,29 +16,29 @@ object IntervalSpec extends Specification {
     (Interval.open(0, 4) borders Interval.open(4, 8)) must beTrue
     (Interval.empty borders Interval.open(4, 8)) must beFalse
   }
-  
+
   "intervals should union properly" in {
-    (Interval.open(0, 4) union Interval.open(4, 8)) must_==(Interval.open(0, 8))
+    (Interval.open(0, 4) union Interval.open(4, 8)) must_== (Interval.open(0, 8))
     (Interval.open(0, 4) union Interval.open(6, 8)) must throwA[IllegalArgumentException]
   }
-  
+
   "intervals should intersect properly" in {
-    (Interval.open(0, 4) intersect Interval.open(4, 8)) must_==(Interval.empty)
-    (Interval.open(0, 4) intersect Interval.open(6, 8)) must_==(Interval.empty)
-    (Interval.open(0, 4) intersect Interval.open(2, 6)) must_==(Interval.open(2, 4))
+    (Interval.open(0, 4) intersect Interval.open(4, 8)) must_== (Interval.empty)
+    (Interval.open(0, 4) intersect Interval.open(6, 8)) must_== (Interval.empty)
+    (Interval.open(0, 4) intersect Interval.open(2, 6)) must_== (Interval.open(2, 4))
   }
-  
+
   "the correct left interval is determined" in {
-    (Interval.open(0, 4) left Interval.open(4, 8)) must_==(Interval.open(0, 4))
-    (Interval.open(0, 4) left Interval.open(2, 6)) must_==(Interval.open(0, 4))
-    (Interval.open(4, 8) left Interval.open(0, 4)) must_==(Interval.open(0, 4))
-    (Interval.open(2, 6) left Interval.open(0, 4)) must_==(Interval.open(0, 4))
+    (Interval.open(0, 4) left Interval.open(4, 8)) must_== (Interval.open(0, 4))
+    (Interval.open(0, 4) left Interval.open(2, 6)) must_== (Interval.open(0, 4))
+    (Interval.open(4, 8) left Interval.open(0, 4)) must_== (Interval.open(0, 4))
+    (Interval.open(2, 6) left Interval.open(0, 4)) must_== (Interval.open(0, 4))
   }
-  
+
   "the correct right interval is determined" in {
-    (Interval.open(0, 4) right Interval.open(4, 8)) must_==(Interval.open(4, 8))
-    (Interval.open(0, 4) right Interval.open(2, 6)) must_==(Interval.open(2, 6))
-    (Interval.open(4, 8) right Interval.open(0, 4)) must_==(Interval.open(4, 8))
-    (Interval.open(2, 6) right Interval.open(0, 4)) must_==(Interval.open(2, 6))
+    (Interval.open(0, 4) right Interval.open(4, 8)) must_== (Interval.open(4, 8))
+    (Interval.open(0, 4) right Interval.open(2, 6)) must_== (Interval.open(2, 6))
+    (Interval.open(4, 8) right Interval.open(0, 4)) must_== (Interval.open(4, 8))
+    (Interval.open(2, 6) right Interval.open(0, 4)) must_== (Interval.open(2, 6))
   }
 }
