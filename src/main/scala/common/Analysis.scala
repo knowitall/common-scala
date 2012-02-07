53873dcf10973a113c41dc87a9bed20e4eb4e86b
Michael Schmitz <michael@schmitztech.com>
2012-02-06 16:48:46 -0800

    Added Analysis.precisionYieldMeta
    Takes a series of metainformation, evaluation pairs and breaks it into
    precision, yield points when the metadatum changes.

--- src/main/scala/common/Analysis.scala
+++ src/main/scala/common/Analysis.scala
@@ -2,34 +2,66 @@ package common.stats
 
 object Analysis {
   /**
-    * Compute precision yield points for each yield value.
-    */
-  def precisionYield(scores: Seq[Boolean]) = {
+   * Compute precision yield points for each yield value.
+   * 
+   * @return  a yield, precision point
+   */
+  def precisionYield(scores: Seq[Boolean]): Seq[(Int, Double)] = {
     var correct = 0
     var incorrect = 0
     var points = List[(Int, Double)]()
-    
+
     for (score <- scores) {
       if (score) correct = correct + 1
       else incorrect = incorrect + 1
-      
+
       if (score) {
         points ::= (correct, precision(correct, incorrect))
       }
     }
-    
+
     points.reverse
   }
   
+   /**
+   * Compute precision yield points for each change in fst.
+   * 
+   * Scored examples are presumed to be in sorted order.
+   * 
+   * @return  a triple of T, the yield, and the precision
+   */
+  def precisionYieldMeta[T](scores: Seq[(T, Boolean)]): Seq[(T, Int, Double)] = {
+    if (scores.length == 0) List()
+    else {
+      var correct = 0
+      var incorrect = 0
+      var points = List[(T, Int, Double)]()
+      var last = scores.head._1
+
+      for ((meta, score) <- scores) {
+        if (score) correct = correct + 1
+        else incorrect = incorrect + 1
+
+        if (meta != last) {
+          last = meta
+          points ::= (meta, correct, precision(correct, incorrect))
+        }
+      }
+
+      points.reverse
+    }
+  }
+
+
   /**
-    * Compute precision from counts of correct and incorrect examples.
-    */
-  def precision(correct: Int, incorrect: Int): Double = 
+   * Compute precision from counts of correct and incorrect examples.
+   */
+  def precision(correct: Int, incorrect: Int): Double =
     (correct.toDouble) / (correct + incorrect).toDouble
-    
+
   /**
-    * Compute precision from a series of evaluations.
-    */
+   * Compute precision from a series of evaluations.
+   */
   def precision(scores: Seq[Boolean]): Double = {
     val correct = scores.count(_ == true)
     precision(correct, scores.size - correct)
